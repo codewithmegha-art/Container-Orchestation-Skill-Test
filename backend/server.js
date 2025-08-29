295b6c37e387513c328ccd8512c29875cb13b3dc
codewithmegha-art <meghadumbre666@gmail.com>
2025-08-29 08:49:35 +0530

    create dockerfile

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
+// Connect to MongoDBs
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
